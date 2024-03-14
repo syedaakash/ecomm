133f95520e98053373ec820ca75d13b1925e3605
syedaakash <syedaakash544@gmail.com>
2024-03-14 11:25:52 +0500

    Serving & fetching data, MongoDB & Mongoose

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,14 +1,26 @@
-import {useParams} from "react-router-dom";
-import products from "../products"
+import { useState, useEffect} from "react";
+import {Link, useParams} from "react-router-dom";
 import {Button, Card, Col, Image, ListGroup, Row} from "react-bootstrap";
 import Rating from "../components/Rating";
+import axios from "axios"
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({});
     const { id: productId } = useParams()
-    const product = products.find((product) => product._id === productId)
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data} = await axios.get(`/api/products/${productId}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [productId])
 
     return (
         <>
+            <Link to="/">
+                <Button className="btn btn-light my-3 px-3">Back</Button>
+            </Link>
             <Row>
                 <Col md={5}>
                     <Image src={product.image} alt={product.name} fluid/>
